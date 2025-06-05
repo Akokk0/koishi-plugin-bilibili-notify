ed6a593b3f593c9d9a45524f32ec5778e78e163e
Akokko <admin@akokko.com>
2025-06-05 20:12:08 +0800

    ver 3.2.5-alpha.2

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
-    format: ['esm', 'cjs'],
-    entry: ['./src'],
-    dts: true,
-    clean: true,
-    outDir: 'lib',
-    loader: { '.jpg': 'asset', '.png': 'base64', '.ttf': 'asset' }
-})
+	format: ["esm", "cjs"],
+	entry: ["./src"],
+	dts: true,
+	clean: true,
+	outDir: "lib",
+	copy: { from: "./src/page", to: "./lib/page" },
+	loader: { ".png": "base64", ".ttf": "asset" },
+});
